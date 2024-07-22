d4585ac3a2b2b53343d03b70817f80035a91cfea
QY <marvinyao@icloud.com>
2024-07-22 02:27:52 +0200

    Better route

--- portfolio/src/app/app.config.ts
+++ portfolio/src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideAnimationsAsync(),]
+    provideAnimationsAsync(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },]
 };
