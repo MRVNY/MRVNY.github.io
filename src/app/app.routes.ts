b81af0e198ad015a1831376e1973378d6f523490
QY <marvinyao@icloud.com>
2025-04-23 14:54:52 +0200

    OTW

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent} from '@components/home/home.component';
 import { OtwComponent } from './otw/otw.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'otw', pathMatch: 'full'},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'otw', component: OtwComponent},
 ];
